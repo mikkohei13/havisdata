fc22f8e43312175b44eaf492df2379e8bca546e6
Mikko Heikkinen <hawk@biomi.org>
2016-05-11 13:51:40 +0300

    Use fopen to read large file line by line

--- parse.php
+++ parse.php
@@ -1,33 +1,51 @@
 <?php
-$filename = "data/tiira-simple.csv";
+$filename = "data/tiira-simple-30.csv";
 
 $filterSpecies = "Emberiza rustica";
 $filterMuni = "Sipoo";
 
-$dataString = file_get_contents($filename);
+//$dataString = file_get_contents($filename);
 
-$dataRows = explode("\n", $dataString);
 
-foreach ($dataRows as $key => $row)
+$handle = fopen($filename, "r") or die("Couldn't get handle");
+if ($handle) {
+    while (!feof($handle)) {
+        $buffer = fgets($handle, 4096);
+        parseBuffer($buffer);
+    }
+    fclose($handle);
+}
+
+
+function parseBuffer($row)
 {
+	global $filterSpecies;
+	global $filterMuni;
+
+	print_r ($row);
+//	exit(); // debug
+
+//	$dataRows = explode("\n", $dataString);
+
 	$cells = explode("\t", $row);
 
 	// species
 	if ($cells[9] != $filterSpecies)
 	{
 //		echo "skipped A:" . $cells[9] . "/<br />\n";
-		continue;
+		return;
 	}
 
 	// muni
 	if ($cells[14] != $filterMuni)
 	{
 //		echo "skipped B:" . $cells[14] . "/<br />\n";
-		continue;
+		return;
 	}
 
 	echo $row;
 	echo "<br />\n";
+
+
 }
 
-// print_r ($dataRows); // debug: print all
