d93638ce95a835e1ba82c0a8c135e4cce37c493b
Mikko Heikkinen <hawk@biomi.org>
2016-05-16 14:39:07 +0300

    Count observations per grid

--- parse.php
+++ parse.php
@@ -3,21 +3,60 @@ $filename = "data/tiira-simple.csv";
 $filename = "data/tiira-simple-30.csv";
 $filename = "data/tiira-simple-awk10000.csv";
 
+/*
+index contents
+9	species
+14	municipality
+16	lat
+17	lon
+23	day
+24	month
+25	year
+33	society name
 
+*/
 //$dataString = file_get_contents($filename);
+
+$gridData = Array();
 
 // Source: http://stackoverflow.com/questions/6830191/reading-huge-file-line-by-line-in-php
 $handle = fopen($filename, "r") or die("Couldn't get handle");
 if ($handle) {
     while (!feof($handle)) {
         $buffer = fgets($handle, 4096);
-        parseBufferFiltered($buffer);
+//        parseBufferFiltered($buffer);
+        parsePerGrid($buffer);
     }
     fclose($handle);
 }
 
+printPlotterFormat($gridData);
+
+print_r ($gridData);
 echo "END";
 
+// Counts how many observations per grid
+function parsePerGrid($row)
+{
+	global $gridData;
+	$cells = explode("\t", $row);
+
+	// Skip if lat or lon is zero
+	if ($cells[16] == 0 || $cells[17] == 0)
+	{
+		return;
+	}
+
+	$gridId = $cells[16] . ":" . $cells[17];
+	@$gridData[$gridId]++;
+}
+
+function printPlotterFormat($gridData)
+{
+	
+}
+
+// Shows only rows that pass the filters
 function parseBufferFiltered($row)
 {
 	$filterSpecies = "Emberiza rustica";
